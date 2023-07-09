88d0adee672b9e321a97a6fb438c5dcaa06f1c69
pakayauttoxxxx <139034310+pakayauttoxxxx@users.noreply.github.com>
2023-07-09 12:56:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94766315540', 'me', true],
+  ['94766315540', 'chami', true], 
   [''] 
 ] //Number of owners
 
